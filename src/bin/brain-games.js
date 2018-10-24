a237211418e06f24af3b23e7012eab146d6c568d
Vasily Varakin <bukawandr@gmail.com>
2018-10-24 20:51:26 +0300

    correct

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 import sayHi from '..';
 
-/* eslint-disable no-console */
 console.log('Welcome to the Brain Games!');
-/* eslint-disable no-console */
 
 sayHi();
--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import readlineSync from 'readline-sync';
 
 export default () => {
   const name = readlineSync.question('May I have you name? ');
-  /* eslint-disable no-console */
   console.log(`Hello, ${name}!`);
-  /* eslint-disable no-console */
 };
